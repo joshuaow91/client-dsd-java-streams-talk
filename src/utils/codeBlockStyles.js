aa084bfbda373f70548a261cf2f6ba9e380534b1
Joshua Ow <joshuaow@gmail.com>
2024-08-29 18:48:32 -0500

    font

--- src/utils/codeBlockStyles.js
+++ src/utils/codeBlockStyles.js
@@ -1,7 +1,7 @@
 export const customStyle =
   window.innerWidth > 768
     ? {
-        fontSize: "1.25em",
+        fontSize: "1.50em",
         padding: "1em",
         borderRadius: "10px",
       }
